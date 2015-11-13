58530be3a8daa593d53623183a1d4be2fc8c2760
Daniel Imhoff <dwieeb@gmail.com>
2015-11-13 11:50:35 -0600

    Cleanup bash hooks

--- ios9_allow_http.sh
+++ ios9_allow_http.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [[ ! -f /usr/libexec/PlistBuddy ]]; then
+    exit 0
+fi
+
 PLIST=platforms/ios/*/*-Info.plist
 
 cat << EOF |
+Delete :NSAppTransportSecurity
 Add :NSAppTransportSecurity dict
 Add :NSAppTransportSecurity:NSAllowsArbitraryLoads bool YES
 EOF
@@ -10,5 +15,3 @@ while read line
 do
     /usr/libexec/PlistBuddy -c "$line" $PLIST
 done
-
-true
--- ios9_allow_native_fb.sh
+++ ios9_allow_native_fb.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+
+if [[ ! -f /usr/libexec/PlistBuddy ]]; then
+    exit 0
+fi
+
 PLIST=platforms/ios/*/*-Info.plist
- 
+
 # Bypass ATS for test servers
 cat << EOF |
 Delete :LSApplicationQueriesSchemes
 Add :LSApplicationQueriesSchemes array
-Add :LSApplicationQueriesSchemes:0 string 'fbapi'                                                             
-Add :LSApplicationQueriesSchemes:1 string 'fbapi20130214'                                                     
-Add :LSApplicationQueriesSchemes:2 string 'fbapi20130410'                                                     
-Add :LSApplicationQueriesSchemes:3 string 'fbapi20130702'                                                     
+Add :LSApplicationQueriesSchemes:0 string 'fbapi'
+Add :LSApplicationQueriesSchemes:1 string 'fbapi20130214'
+Add :LSApplicationQueriesSchemes:2 string 'fbapi20130410'
+Add :LSApplicationQueriesSchemes:3 string 'fbapi20130702'
 Add :LSApplicationQueriesSchemes:4 string 'fbapi20131010'
-Add :LSApplicationQueriesSchemes:5 string 'fbapi20131219'                                                     
-Add :LSApplicationQueriesSchemes:6 string 'fbapi20140410'                                                     
-Add :LSApplicationQueriesSchemes:7 string 'fbapi20140116'                                                     
-Add :LSApplicationQueriesSchemes:8 string 'fbapi20150313'                                                     
-Add :LSApplicationQueriesSchemes:9 string 'fbapi20150629'                                                     
-Add :LSApplicationQueriesSchemes:10 string 'fbauth'                                                            
-Add :LSApplicationQueriesSchemes:11 string 'fbauth2'                                                           
+Add :LSApplicationQueriesSchemes:5 string 'fbapi20131219'
+Add :LSApplicationQueriesSchemes:6 string 'fbapi20140410'
+Add :LSApplicationQueriesSchemes:7 string 'fbapi20140116'
+Add :LSApplicationQueriesSchemes:8 string 'fbapi20150313'
+Add :LSApplicationQueriesSchemes:9 string 'fbapi20150629'
+Add :LSApplicationQueriesSchemes:10 string 'fbauth'
+Add :LSApplicationQueriesSchemes:11 string 'fbauth2'
 EOF
 while read line
 do
-/usr/libexec/PlistBuddy -c "$line" $PLIST
+    /usr/libexec/PlistBuddy -c "$line" $PLIST
 done
- 
-true
