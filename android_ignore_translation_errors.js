372722ec5d073dc7bcc0f7ca04f3a044da835568
carson-drake <carson@greekrush.com>
2015-11-16 14:15:58 -0800

    convert hook to node module.

--- android_ignore_translation_errors.js
+++ android_ignore_translation_errors.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+
 
 // add additional build-extras.gradle file to instruct android's lint to ignore translation errors
 // v0.1.c
@@ -6,25 +6,35 @@
 // Issue: https://github.com/phonegap/phonegap-plugin-barcodescanner/issues/80
 //
 // Warning: This solution does not solve the problem only makes it possible to build --release
-
-var fs = require('fs');
-
-var rootdir = process.argv[2];
-
-
-if(rootdir){
+module.exports = function(ctx){
+	// make sure android platform is part of build 
+	if(ctx.opts.platforms.indexOf('android') < 0){
+		return;
+	}
+	var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        deferral = ctx.requireCordovaModule('q').defer();
 	
-	var platforms = (process.env.CORDOVA_PLATFORMS ? process.env.CORDOVA_PLATFORMS.split(',') : []);
-	for(var x = 0; x < platforms.length; x++){
-		var platform = platforms[x].trim().toLowerCase();
-		try{
-			if(platform == 'android'){
-				var lintOptions = 'android { \nlintOptions {\ndisable \'MissingTranslation\' \ndisable \'ExtraTranslation\' \n} \n}';
-				fs.appendFileSync('platforms/android/build-extras.gradle', lintOptions, 'UTF-8');
-				process.stdout.write('Added build-extras.gradle ');
+	var rootdir = path.join(ctx.opts.projectRoot, 'platforms/android');
+	
+	
+	if(rootdir){
+		
+		var platforms = ctx.opts.platforms;
+		for(var x = 0; x < platforms.length; x++){
+			var platform = platforms[x].trim().toLowerCase();
+			try{
+				if(platform == 'android'){
+					var lintOptions = 'android { \nlintOptions {\ndisable \'MissingTranslation\' \ndisable \'ExtraTranslation\' \n} \n}';
+					fs.appendFileSync('platforms/android/build-extras.gradle', lintOptions, 'UTF-8');
+					console.log('Added build-extras.gradle ');
+					deferral.resolve();
+				}
 			}
-		}catch(e){
-			process.stdout.write(e);
+			catch(e){
+				console.log(e);
+			}
 		}
 	}
-}
+	return deferral.promise;
+};
