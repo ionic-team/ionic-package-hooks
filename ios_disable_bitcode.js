b20e05656dcde4f0cbbb3aff07241ff5b6efa69a
Daniel Imhoff <dwieeb@gmail.com>
2016-01-04 16:37:02 -0600

    Refactor into module

--- ios_disable_bitcode.js
+++ ios_disable_bitcode.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /**
  * Disable bitcode in xcode builds
  * Credit to akofman's initial version: https://gist.github.com/akofman/b53124f76ec309160e08
@@ -7,18 +5,30 @@
 
 var xcode = require('xcode');
 var fs = require('fs');
-var cordova_util = require('cordova-lib/src/cordova/util');
-var ConfigParser = require('cordova-common').ConfigParser;
 
-var projectName = new ConfigParser(cordova_util.projectConfig(cordova_util.isCordova())).name();
-var projectPath = './platforms/ios/' + projectName + '.xcodeproj/project.pbxproj';
-var myProj = xcode.project(projectPath);
-
-myProj.parse(function(err) {
-  if (err){
-    console.log('Error: ' + JSON.stringify(err));
-  } else{
-    myProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
-    fs.writeFileSync(projectPath, myProj.writeSync());
+module.exports = function(ctx) {
+  if (ctx.opts.platforms.indexOf('ios') < 0) {
+    return;
   }
-});
+
+  var deferral = ctx.requireCordovaModule('q').defer();
+  var common = ctx.requireCordovaModule('cordova-common');
+  var util = ctx.requireCordovaModule('cordova-lib/src/cordova/util');
+
+  var projectName = new common.ConfigParser(util.projectConfig(util.isCordova())).name();
+  var projectPath = './platforms/ios/' + projectName + '.xcodeproj/project.pbxproj';
+  var p = xcode.project(projectPath);
+
+  p.parse(function(err) {
+    if (err) {
+      console.error(err);
+      deferral.reject('xcode could not parse project');
+    } else{
+      p.updateBuildProperty('ENABLE_BITCODE', 'NO');
+      fs.writeFileSync(projectPath, p.writeSync());
+      deferral.resolve();
+    }
+  });
+
+  return deferral.promise;
+};
