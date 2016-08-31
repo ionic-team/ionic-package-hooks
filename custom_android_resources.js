18d052338097ec35265f27c78f2ce3f436e6c2bf
Daniel Imhoff <dwieeb@gmail.com>
2016-08-31 15:21:49 -0500

    General format cleanup

--- custom_android_resources.js
+++ custom_android_resources.js
@@ -1,54 +1,53 @@
 #!/usr/bin/env node
 
-var source = "resources/android/custom/";
-var android_dests = [
-  "res/drawable-ldpi-v4/",
-  "res/drawable-mdpi-v4/",
-  "res/drawable-hdpi-v4/",
-  "res/drawable-xhdpi-v4/",
-  "res/drawable-xxhdpi-v4/",
-  "res/drawable-xxxhdpi-v4/",
-  "res/drawable-ldpi/",
-  "res/drawable-mdpi/",
-  "res/drawable-hdpi/",
-  "res/drawable-xhdpi/",
-  "res/drawable-xxhdpi/",
-  "res/drawable-xxxhdpi/"
+var fs = require('fs');
+var path = require('path');
+
+var platformDir = 'platforms/android';
+var source = 'resources/android/custom/';
+var resourceDirs = [
+  'res/drawable-ldpi-v4/',
+  'res/drawable-mdpi-v4/',
+  'res/drawable-hdpi-v4/',
+  'res/drawable-xhdpi-v4/',
+  'res/drawable-xxhdpi-v4/',
+  'res/drawable-xxxhdpi-v4/',
+  'res/drawable-ldpi/',
+  'res/drawable-mdpi/',
+  'res/drawable-hdpi/',
+  'res/drawable-xhdpi/',
+  'res/drawable-xxhdpi/',
+  'res/drawable-xxxhdpi/'
 ];
 
+function copy(src, dest, file) {
+  var deferred = Q.defer();
+
+  if (fs.existsSync(src) && fs.existsSync(dest)) {
+    deferred.resolve(fs.createReadStream(src).pipe(fs.createWriteStream(path.join(dest, file))));
+  } else {
+    deferred.resolve();
+  }
+
+  return deferred.promise;
+}
 
 module.exports = function(ctx) {
   if (ctx.opts.platforms.indexOf('android') < 0) {
     return;
   }
 
-  var fs = ctx.requireCordovaModule('fs');
-  var path = ctx.requireCordovaModule('path');
   var Q = ctx.requireCordovaModule('q');
-
-  var android_rootdir = path.join(ctx.opts.projectRoot, 'platforms/android');
-
-  function copy(src, dest_dir, file) {
-    var deferred = Q.defer();
-
-    if (fs.existsSync(src) && fs.existsSync(dest_dir)) {
-      deferred.resolve(fs.createReadStream(src).pipe(fs.createWriteStream(path.join(dest_dir, file))));
-    } else {
-      deferred.resolve();
-    }
-
-    return deferred.promise;
-  }
-
-  var promise = Q(-1);
+  var deferred = Q.defer();
+  var androidPlatformDir = path.join(ctx.opts.projectRoot, platformDir);
 
   fs.readdir(path.join(ctx.opts.projectRoot, source), function(err, files) {
     files.forEach(function(file) {
-      android_dests.forEach(function(destination) {
-        return copy(path.join(ctx.opts.projectRoot, source + file), path.join(android_rootdir, destination), file);
+      resourceDirs.forEach(function(destination) {
+        return copy(path.join(ctx.opts.projectRoot, source + file), path.join(androidPlatformDir, destination), file);
       });
     });
   });
 
-  return promise;
+  return deferred.promise;
 }
