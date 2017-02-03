94c406f8473d93328efcd4e99045b99c07cbfc84
rolandjitsu <rolandjitsu@gmail.com>
2017-02-03 17:32:18 +0200

    Add credits for the hook

--- copy_google_services_config_files.js
+++ copy_google_services_config_files.js
@@ -5,6 +5,7 @@
 /**
  * This hook makes sure projects using [cordova-plugin-firebase](https://github.com/arnesson/cordova-plugin-firebase)
  * will build properly and have the required key files copied to the proper destinations when the app is build on Ionic Cloud using the package command.
+ * Credits: https://github.com/arnesson.
  */
 
 
